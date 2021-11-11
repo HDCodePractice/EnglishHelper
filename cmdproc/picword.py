cec6a5448b56b05c8886cd52fb0b252cf7065784
Danny Huang <huangdong@gmail.com>
2021-11-10 23:38:24 -0500

    加入了单词提示功能

--- cmdproc/picword.py
+++ cmdproc/picword.py
@@ -1,6 +1,5 @@
-from telegram import Update, BotCommand
-from telegram.ext import CommandHandler,CallbackContext
-from telegram.ext import MessageHandler, Filters
+from telegram import Update, BotCommand, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler,CallbackContext,CallbackQueryHandler
 from json import load
 from config import ENV
 import random
@@ -10,25 +9,76 @@ with open('pic_dict.json','r') as wd:
     word_dict = load(wd)
 
 def check_answer(question,answer,filenumber):
+    # 问题的答案是否正确
+    # question : 图中的号码
+    # answer : 用户回答的答案
+    # filenumber : 图片的编号
     if answer.lower() in word_dict:
         words = word_dict[answer.lower()]
         for word in words:
-            print(word,question,filenumber)
             if question == word["number"] and f"{filenumber}.jpg" == word["filename"]:
                 return True
     return False
 
+def get_show_word(word,show_count):
+    # 将一个单词的字母显示出来show_count个
+    # 如 word = "hello" show_count = 3 则返回 "h**lo"
+    show_word = ""
+    for i in word:
+        if i == " ":
+            show_word += " "
+        else:
+            show_word += "*"
+    for i in range(show_count):
+        b = i + 1
+        if b % 2 == 0: # 偶数，后面加提示
+            b =  - b // 2
+            if b == -1:
+                show_word = show_word[:b] + word[b]
+            else:
+                show_word = show_word[:b] + word[b] + show_word[b+1:]
+        else:
+            b = b // 2
+            show_word = show_word[:b] + word[b] + show_word[b+1:]
+    return show_word
+
 def remember_command(update: Update, context: CallbackContext) -> None:
     if str(update.effective_chat.id) not in ENV.CHATIDS:
         return
-    word = random.choice(list(word_dict.keys()))
-    word = random.choice(word_dict[word])
+    rword = random.choice(list(word_dict.keys()))
+    word = random.choice(word_dict[rword])
     filenumber = word["filename"].split(".")[0]
     filename = f"res/picwords/{word['filename']}"
     number = word["number"]
     msg = f"图中的{number}\n是什么单词或短语\n请回复本消息回答你的答案。\nPage:{filenumber}"
-    update.message.reply_photo(photo=open(filename,'rb'),caption=msg)
+    buttons = [[InlineKeyboardButton("跪求一个字母的提示",callback_data=f"rhit:{number}:{filenumber}:{rword}:0")]]
+    update.message.reply_photo(
+        photo=open(filename,'rb'),
+        caption=msg,
+        reply_markup=InlineKeyboardMarkup(buttons))
+
+def remember_hit_callback(update: Update, context: CallbackContext) -> None:
+    if str(update.effective_chat.id) not in ENV.CHATIDS:
+        return
+    query = update.callback_query
+    data = query.data.split(":")
+    if len(data) != 5:
+        return
+    keyboard = query.message.reply_markup
+    msgs = query.message.caption.split("\n")
+    word = data[3]
+    show_count = int(data[4])+1
+    show_word = get_show_word(word,show_count)
+    msg = msgs[0] + f"\n是什么单词或短语，提示：{show_word}\n" + msgs[2] + "\n" + msgs[3]
+    keyboard.inline_keyboard[0][0].callback_data = f"rhit:{data[1]}:{data[2]}:{data[3]}:{show_count}"
+    if show_count < len(word):
+        update.callback_query.edit_message_caption(msg,reply_markup=keyboard)
+        query.answer("又多给你一个字母！")
+    else:
+        update.callback_query.edit_message_caption(msg + "\n唉，没想到这一群人，竟都不知道如此简单的单词！真令人失望啊～",reply_markup=None)
+        query.answer("全部答案都给你啦！老子家底都被你掏空了！",show_alert=True)
 
 def add_dispatcher(dp):
     dp.add_handler(CommandHandler("m", remember_command))
+    dp.add_handler(CallbackQueryHandler(remember_hit_callback,pattern="^rhit:[A-Za-z0-9_]*"))
     return [BotCommand("m", "看图想词游戏")]
