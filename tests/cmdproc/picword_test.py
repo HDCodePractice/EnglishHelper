e0b4b7f057dbb645488e4c14a71793d2ac253052
Danny Huang <huangdong@gmail.com>
2021-11-13 17:48:59 -0500

    加入第二个单元测试

--- tests/cmdproc/picword_test.py
+++ tests/cmdproc/picword_test.py
@@ -1,8 +1,14 @@
 import pytest
 
-def testget_show_word():
+def test_get_show_word():
     from cmdproc.picword import get_show_word
     assert get_show_word("test",1) == "t***"
     assert get_show_word("test",2) == "t**t"
     assert get_show_word("test",3) == "te*t"
     assert get_show_word("test",4) == "test"
+
+def test_check_answer():
+    from cmdproc.picword import check_answer
+    assert check_answer("3","neck","104") == True
+    assert check_answer("3","Neck","104") == True
+    assert check_answer("3","Necka","104") == False
