2a3528d5b4fd466988df3649b93a23bdf3b2632e
Danny Huang <huangdong@gmail.com>
2021-11-14 17:50:48 -0500

    生成了chapter_dick.json用于查询chapter的关系

--- csvtojson.py
+++ csvtojson.py
@@ -4,9 +4,9 @@ from pybadges import badge
 
 word_dict= {}
 
-def save_word_dict(filename):
+def save_word_dict(filename,words_dict):
     with open(filename, 'w') as configfile:
-        dump(word_dict, configfile, indent=2,ensure_ascii=False)
+        dump(words_dict, configfile, indent=2,ensure_ascii=False)
 
 with open('iverbs.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -28,7 +28,6 @@ with open('iverbs.csv', 'r') as csvfile:
                         if d not in word_dict[word]: # 如果单词在word_dict中，但是说明不在word_dict[word]中
                             word_dict[word].append(d)
 
-
 with open('inouns.csv','r') as csvnounfile:
     reader = csv.DictReader(csvnounfile)
     for row in reader:
@@ -46,31 +45,46 @@ with open('inouns.csv','r') as csvnounfile:
                     if word not in word_dict:
                         word_dict[word] = [g]
 print(f"Irregular单词条目：{len(word_dict)}个")
-save_word_dict("word_dict.json")
+save_word_dict("word_dict.json",word_dict)
 s = badge(left_text="Irregular Words",right_text=f"{len(word_dict)}")
 with open('irregular.svg', 'w') as f:
     f.write(s)
 
 word_dict={}
+chapter_dict={}
 
 with open('picwords.csv','r') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         # row格式为：{'File Name': '86.jpg', 'Chapter': 'Clothing', 'Topic': 'Everyday Clothes', 'words': '1.shirt|2.jeans|3.dress|4.T-shirt|5.baseball cap|6.socks|7.athletic shoes|A.tie|8.blouse|9.handbag|10.skirt|11.suit|12.slacks/pants|13.shoes|14.sweater|B.put on'}
         filename = row['File Name']
+        filenumber = filename.split('.')[0]
         chapter = row['Chapter']
         topic = row['Topic']
         words = row['words']
         for word in words.split('|'):  # 切出所有的带number的单词
             num , pre_word = word.split('.')  # 切出number和单词
             for word in pre_word.split('/'):    # 一个图有多个单词会使用/分割
+                # 将单词说明添加到word_dict中
                 if word not in word_dict:   # 如果单词不在word_dict中
                     word_dict[word.lower()] = [{'chapter':chapter,'topic':topic,'filename':filename,'number':num}]
                 else:
                     word_dict[word.lower()].append({'chapter':chapter,'topic':topic,'filename':filename,'number':num})
+                # 将单词说明添加到chapter_dict中
+                if chapter not in chapter_dict:
+                    chapter_dict[chapter] = {}
+                if topic not in chapter_dict[chapter]:
+                    chapter_dict[chapter][topic] = {}
+                if filenumber not in chapter_dict[chapter][topic]:
+                    chapter_dict[chapter][topic][filenumber] = {}
+                if num not in chapter_dict[chapter][topic][filenumber]:
+                    chapter_dict[chapter][topic][filenumber][num] = [word]
+                if word not in chapter_dict[chapter][topic][filenumber][num]:
+                    chapter_dict[chapter][topic][filenumber][num].append(word)
 
 print(f"看图识词单词条目：{len(word_dict)}个")
-save_word_dict("pic_dict.json")
+save_word_dict("pic_dict.json",word_dict)
+save_word_dict("chapter_dict.json",chapter_dict)
 s = badge(left_text="Picture Words",right_text=f"{len(word_dict)}")
 with open('picturewords.svg', 'w') as f:
     f.write(s)
