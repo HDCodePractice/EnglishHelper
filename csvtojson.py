e62943c8b9e392e7ac8d4b8d3f9e9fa980d15c6f
Danny Huang <huangdong@gmail.com>
2021-11-14 21:15:26 -0500

    将pic csv to json 抽像出来以方便复用

--- csvtojson.py
+++ csvtojson.py
@@ -1,5 +1,5 @@
 import csv
-from utils.fileproc import save_word_dict
+from utils.fileproc import save_word_dict, gen_pic_json_from_csv
 from pybadges import badge
 
 word_dict= {}
@@ -50,34 +50,4 @@ word_dict={}
 chapter_dict={}
 
 with open('res/picwords.csv','r') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        # row格式为：{'File Name': '86.jpg', 'Chapter': 'Clothing', 'Topic': 'Everyday Clothes', 'words': '1.shirt|2.jeans|3.dress|4.T-shirt|5.baseball cap|6.socks|7.athletic shoes|A.tie|8.blouse|9.handbag|10.skirt|11.suit|12.slacks/pants|13.shoes|14.sweater|B.put on'}
-        filename = row['File Name']
-        filenumber = filename.split('.')[0]
-        chapter = row['Chapter']
-        topic = row['Topic']
-        words = row['words']
-        for word in words.split('|'):  # 切出所有的带number的单词
-            num , pre_word = word.split('.')  # 切出number和单词
-            for word in pre_word.split('/'):    # 一个图有多个单词会使用/分割
-                # 将单词说明添加到word_dict中
-                if word not in word_dict:   # 如果单词不在word_dict中
-                    word_dict[word.lower()] = [{'chapter':chapter,'topic':topic,'filename':filename,'number':num}]
-                else:
-                    word_dict[word.lower()].append({'chapter':chapter,'topic':topic,'filename':filename,'number':num})
-                # 将单词说明添加到chapter_dict中
-                if chapter not in chapter_dict:
-                    chapter_dict[chapter] = {}
-                if topic not in chapter_dict[chapter]:
-                    chapter_dict[chapter][topic] = {}
-                if filenumber not in chapter_dict[chapter][topic]:
-                    chapter_dict[chapter][topic][filenumber] = {}
-                if num not in chapter_dict[chapter][topic][filenumber]:
-                    chapter_dict[chapter][topic][filenumber][num] = [word]
-                if word not in chapter_dict[chapter][topic][filenumber][num]:
-                    chapter_dict[chapter][topic][filenumber][num].append(word)
-
-print(f"看图识词单词条目：{len(word_dict)}个")
-save_word_dict("pic_dict.json",word_dict)
-save_word_dict("chapter_dict.json",chapter_dict)
+    gen_pic_json_from_csv(csvfile)
