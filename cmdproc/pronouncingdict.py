57bdec8c30ef5feed4ee4c7637bbf903fee0935a
Danny Huang <huangdong@gmail.com>
2021-11-17 17:22:26 -0500

    根据undeadkylin的建议,button加上dictionary，更清晰

--- cmdproc/pronouncingdict.py
+++ cmdproc/pronouncingdict.py
@@ -34,7 +34,7 @@ def get_answer(word):
             f"youglish",
             url=f"https://youglish.com/pronounce/{word}/english/us?")],
         [InlineKeyboardButton(
-            f"urban",
+            f"urban dictionary",
             url=f"https://www.urbandictionary.com/define.php?term={word}")],
         [InlineKeyboardButton(
             f"youtube pronunciation",
