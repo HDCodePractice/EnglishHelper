23ecd47cdfa6535c82089c87cfc76f71e70126d4
Danny Huang <huangdong@gmail.com>
2021-11-18 21:57:23 -0500

    变更一个有歧义的函数名，同时让所有的test都通过

--- csvtojson.py
+++ csvtojson.py
@@ -1,5 +1,5 @@
 import csv
-from utils.fileproc import save_word_dict, gen_pic_json_from_csv, gen_irregular_dict_from_csv
+from utils.fileproc import save_word_dict, gen_pic_dict_from_csv, gen_irregular_dict_from_csv
 from pybadges import badge
 
 with open('res/iverbs.csv', 'r') as iverb_csvfile:
@@ -15,7 +15,7 @@ with open('res/iverbs.csv', 'r') as iverb_csvfile:
 with open('res/picwords.csv','r') as csvfile:
     word_dict={}
     chapter_dict={}
-    word_dict,chapter_dict = gen_pic_json_from_csv(csvfile)
+    word_dict,chapter_dict = gen_pic_dict_from_csv(csvfile)
     print(f"看图识词单词条目：{len(word_dict)}个")
     save_word_dict("pic_dict.json",word_dict)
     save_word_dict("chapter_dict.json",chapter_dict)
