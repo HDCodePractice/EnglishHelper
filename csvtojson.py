dceff6cf51056514abe0d3d725fcfc67462ed864
Danny Huang <huangdong@gmail.com>
2021-11-18 21:30:55 -0500

    变量放到局部变量去

--- csvtojson.py
+++ csvtojson.py
@@ -2,10 +2,9 @@ import csv
 from utils.fileproc import save_word_dict, gen_pic_json_from_csv, gen_irregular_dict_from_csv
 from pybadges import badge
 
-word_dict= {}
-
 with open('res/iverbs.csv', 'r') as iverb_csvfile:
     with open('res/inouns.csv','r') as inouns_csvfile:
+        word_dict= {}
         word_dict=gen_irregular_dict_from_csv(iverb_csvfile,inouns_csvfile)
         print(f"Irregular单词条目：{len(word_dict)}个")
         save_word_dict("word_dict.json",word_dict)
@@ -13,10 +12,9 @@ with open('res/iverbs.csv', 'r') as iverb_csvfile:
         with open('irregular.svg', 'w') as f:
             f.write(s)
 
-word_dict={}
-chapter_dict={}
-
 with open('res/picwords.csv','r') as csvfile:
+    word_dict={}
+    chapter_dict={}
     word_dict,chapter_dict = gen_pic_json_from_csv(csvfile)
     print(f"看图识词单词条目：{len(word_dict)}个")
     save_word_dict("pic_dict.json",word_dict)
