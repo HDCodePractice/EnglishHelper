8e05f8553b24c6e38b62d83835c04bbff9dcf5cf
Danny Huang <huangdong@gmail.com>
2021-11-19 20:37:15 -0500

    去除可能涉及版权的图片

--- cmdproc/picword.py
+++ cmdproc/picword.py
@@ -5,6 +5,7 @@ from config import ENV
 import random
 from utils.filters import check_chatid_filter
 from utils.fileproc import gen_pic_dict_from_csv
+from pathlib import Path
 
 word_dict = {}
 chapter_dict = {}
@@ -82,7 +83,11 @@ def remember_command(update: Update, context: CallbackContext) -> None:
     rword = random.choice(list(word_dict.keys()))
     word = random.choice(word_dict[rword])
     filenumber = word["filename"].split(".")[0]
-    filename = f"res/picwords/{word['filename']}"
+    filename = f"{ENV.DATA_DIR}/res/picwords/{word['filename']}"
+    if not Path(filename).is_file():
+        filename = f"/res/picwords/{word['filename']}"
+        if not Path(filename).is_file():
+            update.effective_message.reply_text(f"图片文件{word['filename']}不存在，请检你的字典")
     number = word["number"]
     show_word = get_show_word(rword,0)
     msg = f"☝️What's #{number}\nHints💡: {show_word}\nPage:{filenumber}\nReply this msg to submit the answer"
