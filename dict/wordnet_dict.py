4b1ae31eab802d2ff7edf0b03081603ab121e8b8
Danny Huang <huangdong@gmail.com>
2021-11-22 18:17:38 -0500

    为说明和例句做更好的展示格式化处理

--- dict/wordnet_dict.py
+++ dict/wordnet_dict.py
@@ -16,58 +16,38 @@ def download_wordnet():
         nltk.download("wordnet", download_dir=ENV.NLTK_DATA_DIR)
 
 
+def get_definition_examples(word, pos):
+    ws = wn.synsets(word, pos=pos)
+    d = ""
+    count = 1
+    for w in ws:
+        d += f"{count}. {w.definition()}\n"
+        if len(w.examples()) > 0:
+            examples = w.examples()
+            e = random.choice(examples)
+            if e[-1] in ["?", "!"]:
+                d += f"E: {e}\n"
+            else:
+                d += f"E: {e}.\n"
+        count += 1
+        if count > 5:
+            break
+    return d[:-1]
+
+
 def dict(word):
     description = ""
-    ws = wn.synsets(word, pos=wn.NOUN)
-    d = ""
-    count = 1
-    for w in ws:
-        d += f"{count}.{w.definition()}\n"
-        if len(w.examples()) > 0:
-            d += f"E: {random.choice(w.examples())}\n"
-        count += 1
-        if count > 5:
-            break
+    d = get_definition_examples(word, pos=wn.NOUN)
     if len(d) > 0:
-        description += f"noun:\n{d}\n"
-
-    ws = wn.synsets(word, pos=wn.VERB)
-    d = ""
-    count = 1
-    for w in ws:
-        d += f"{count}.{w.definition()}\n"
-        if len(w.examples()) > 0:
-            d += f"E: {random.choice(w.examples())}\n"
-        count += 1
-        if count > 5:
-            break
+        description += f"noun:\n{d}\n\n"
+    d = get_definition_examples(word, pos=wn.VERB)
     if len(d) > 0:
-        description += f"verb:\n{d}\n"
-
-    ws = wn.synsets(word, pos=wn.ADJ)
-    d = ""
-    count = 1
-    for w in ws:
-        d += f"{count}.{w.definition()}\n"
-        if len(w.examples()) > 0:
-            d += f"E: {random.choice(w.examples())}\n"
-        count += 1
-        if count > 5:
-            break
+        description += f"verb:\n{d}\n\n"
+    d = get_definition_examples(word, pos=wn.ADJ)
     if len(d) > 0:
-        description += f"adj:\n{d}\n"
-
-    ws = wn.synsets(word, pos=wn.ADV)
-    d = ""
-    count = 1
-    for w in ws:
-        d += f"{count}.{w.definition()}\n"
-        if len(w.examples()) > 0:
-            d += f"E: {random.choice(w.examples())}\n"
-        count += 1
-        if count > 5:
-            break
+        description += f"adj:\n{d}\n\n"
+    d = get_definition_examples(word, pos=wn.ADV)
     if len(d) > 0:
-        description += f"adv:\n{d}\n"
+        description += f"adv:\n{d}\n\n"
 
     return description[:-1]
--- tests/dict/wordnet_dict_test.py
+++ tests/dict/wordnet_dict_test.py
@@ -1,4 +1,5 @@
 from config import ENV
+from nltk.corpus import wordnet as wn
 
 from dict import wordnet_dict
 
@@ -12,3 +13,13 @@ def test_dict(shared_datadir):
     # print(d)
     assert d.find("noun:") == 0
     assert d.find("verb:") > 0
+
+
+def test_get_definition_examples(shared_datadir):
+    ENV.NLTK_DATA_DIR = f"{shared_datadir}/wordnet"
+    wordnet_dict.download_wordnet()
+    d = wordnet_dict.get_definition_examples("like", pos=wn.NOUN)
+    # assert d.find("noun") == 0
+    d = wordnet_dict.get_definition_examples("look", pos=wn.VERB)
+    print(d)
+    # assert d.find("verb") == 0
