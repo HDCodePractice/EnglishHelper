742dfdfa997fa80ce79d95324cb78b4a2ca750da
Danny Huang <huangdong@gmail.com>
2021-11-27 20:52:15 -0500

    结合wiki来显示我们语法内容

--- cmdproc/grammar_cmd.py
+++ cmdproc/grammar_cmd.py
@@ -4,6 +4,7 @@ from config import ENV
 from dict import grammar_dict
 from telegram import BotCommand, InlineKeyboardMarkup, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler
+from telegram.utils.helpers import escape_markdown
 from utils.filters import check_chatid_filter
 
 
@@ -15,7 +16,7 @@ def rgrammar_callback(update: Update, context: CallbackContext):
     msg = grammar_dict.get_grammar(g)
     buttons = grammar_dict.get_grammar_button_list(
         "gram:", exclude=[g])
-    query.edit_message_text(text=msg, parse_mode="HTML",
+    query.edit_message_text(text=msg, parse_mode="MarkdownV2",
                             reply_markup=InlineKeyboardMarkup(buttons))
 
 
@@ -23,9 +24,9 @@ def rgrammar_callback(update: Update, context: CallbackContext):
 def grammar_command(update: Update, context: CallbackContext) -> None:
     buttons = grammar_dict.get_grammar_button_list("gram:")
     update.effective_message.reply_text(
-        "English is simple,remeber <b>S</b>(subject)<b>V</b>(verb)[<b>O</b>(object)]",
+        "English is simple,remeber **S**\(subject\)**V**\(verb\)[**O**\(object\)]",
         reply_markup=InlineKeyboardMarkup(buttons),
-        quote=False, parse_mode="HTML"
+        quote=False, parse_mode="MarkdownV2"
     )
 
 
