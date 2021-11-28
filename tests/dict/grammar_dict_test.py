9d54d46f964890589b95be18da4b7805ee7b9af0
Danny Huang <huangdong@gmail.com>
2021-11-28 09:22:34 -0500

    more unit test

--- tests/dict/grammar_dict_test.py
+++ tests/dict/grammar_dict_test.py
@@ -9,7 +9,12 @@ def test_get_grammar_list():
 
 def test_get_grammar():
     assert len(grammar_dict.get_grammar('Past Continuous')) > 0
+    assert grammar_dict.get_grammar('papapa') is None
 
 
 def test_check_extra_dict(shared_datadir):
     assert grammar_dict.check_extra_dict(shared_datadir) == 0
+
+
+def test_get_grammar_button_list():
+    assert len(grammar_dict.get_grammar_button_list("gram:")) > 0
