c6a53597ae308978c7371d90458de5fea87c49a7
Danny Huang <huangdong@gmail.com>
2021-12-04 20:50:49 -0500

    这个函数被注释，也注释test

--- tests/cmdproc/wordpic_test.py
+++ tests/cmdproc/wordpic_test.py
@@ -1,5 +1,5 @@
-def test_check_answer():
-    from cmdproc.wordpic import check_answer
-    assert check_answer("slash", "5", "hd001") == True
-    assert check_answer("slasha", "5", "hd001") == False
-    assert check_answer("5", "slasha", "hd001") == False
+# def test_check_answer():
+#     from cmdproc.wordpic import check_answer
+#     assert check_answer("slash", "5", "hd001") == True
+#     assert check_answer("slasha", "5", "hd001") == False
+#     assert check_answer("5", "slasha", "hd001") == False
