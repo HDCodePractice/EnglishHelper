8274abf25b00cdbbb03b8580eda2e222198809c9
Danny Huang <huangdong@gmail.com>
2021-12-10 22:22:28 -0500

    统一加载测试数据

---
+++ tests/dict/picture_dict_test.py
@@ -0,0 +1,6 @@
+
+
+def test_reload_dict():
+    from dict import picture_dict
+    picture_dict.reload_dict()
+    print(picture_dict.word_dict)
--- tests/utils/fileproc_test.py
+++ tests/utils/fileproc_test.py
@@ -4,21 +4,21 @@ from utils import fileproc
 
 
 def test_gen_irregular_dict_from_csv(shared_datadir):
-    with open(f"{shared_datadir}/inouns.csv") as inouns_csvfile:
-        with open(f"{shared_datadir}/iverbs.csv", 'r') as iverb_csvfile:
+    with open(f"{shared_datadir}/ext/inouns.csv") as inouns_csvfile:
+        with open(f"{shared_datadir}/ext/iverbs.csv", 'r') as iverb_csvfile:
             word_dict = fileproc.gen_irregular_dict_from_csv(
                 iverb_csvfile, inouns_csvfile)
             assert len(word_dict) == 13
 
 
 def test_gen_pic_dict_from_csv(shared_datadir):
-    with open(f"{shared_datadir}/picwords.csv") as csvfile:
+    with open(f"{shared_datadir}/ext/picwords.csv") as csvfile:
         word_dict, chapter_dict = fileproc.gen_pic_dict_from_csv(csvfile)
-        assert len(word_dict) == 25
+        assert len(word_dict) == 3
         assert len(chapter_dict) == 1
 
 
 def test_gen_grammar_dict_from_csv(shared_datadir):
-    with open(f"{shared_datadir}/grammar.csv") as csvfile:
+    with open(f"{shared_datadir}/ext/grammar.csv") as csvfile:
         word_dict = fileproc.gen_grammar_dict_from_csv(csvfile)
         assert len(word_dict) > 0
