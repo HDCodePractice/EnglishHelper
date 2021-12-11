44cd94361245a9587d1eef39681e1e67f557fb68
Danny Huang <huangdong@gmail.com>
2021-12-10 21:20:20 -0500

    将download_wordnet_data不放在module里

--- tests/dict/conftest.py
+++ tests/dict/conftest.py
@@ -1,11 +1,2 @@
 import pytest
 from config import ENV
-
-from dict import wordnet_dict
-
-
-# each test runs on cwd to its temp dir
-@pytest.fixture(autouse=True)
-def download_wordnet_data(shared_datadir):
-    ENV.NLTK_DATA_DIR = f"{shared_datadir}/wordnet"
-    wordnet_dict.download_wordnet()
