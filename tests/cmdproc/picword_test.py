402f2b5cb3fa581fb6bacb2e42b015dc746626dd
Danny Huang <huangdong@gmail.com>
2021-12-11 19:56:05 -0500

    将测试用例也移到picture_dict_test中

--- tests/cmdproc/picword_test.py
+++ tests/cmdproc/picword_test.py
@@ -8,10 +8,3 @@ def test_get_show_word():
     assert get_show_word("test", 2) == "t**t"
     assert get_show_word("test", 3) == "te*t"
     assert get_show_word("test", 4) == "test"
-
-
-def test_check_answer():
-    from cmdproc.picword import check_answer
-    assert check_answer("5", "slash", "punctuation1") == True
-    assert check_answer("5", "slash", "punctuation1") == True
-    assert check_answer("5", "slasha", "punctuation1") == False
