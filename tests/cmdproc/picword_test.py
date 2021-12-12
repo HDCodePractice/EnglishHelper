5a2b6688c5325d1b6fa28b7073a6c19584e99f4e
Danny Huang <huangdong@gmail.com>
2021-12-11 20:12:52 -0500

    重构继续
    #72 #66

--- tests/cmdproc/picword_test.py
+++ tests/cmdproc/picword_test.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def test_get_show_word():
-    from cmdproc.picword import get_show_word
+    from cmdproc.spell_picword_cmd import get_show_word
     assert get_show_word("test", 0) == "****"
     assert get_show_word("test", 1) == "t***"
     assert get_show_word("test", 2) == "t**t"
