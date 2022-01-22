0159954cf26c36f697905747d2b85b97f9a92723
老房东 <huangdong@gmail.com>
2022-01-21 22:17:31 -0500

    方便使用，变更了名称

--- utils/fileproc.py
+++ utils/fileproc.py
@@ -135,7 +135,8 @@ def gen_picture_dict_from_csv(csvfile, picwords_dict=[]):
                 file_name = key
                 words = topic_dict[key]
                 file_name_list.append({'name': file_name, 'words': words})
-            topic_list.append({'name': topic_name, 'files': file_name_list})
+            topic_list.append(
+                {'name': topic_name, 'pictureFiles': file_name_list})
         picwords_dict.append({'name': chapter_name, 'topics': topic_list})
     return picwords_dict
 
