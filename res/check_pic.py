3a7701526b440fcf0aa3993fd54f947feddc6faf
sunnypo <siyuan0202@gmail.com>
2022-01-24 23:27:06 -0500

    Create check_pic.py

---
+++ res/check_pic.py
@@ -0,0 +1,52 @@
+import csv
+from pathlib import Path
+
+
+# res/pictures/Health/Symptoms and Injuries
+# res/pictures/chapter/topic/words/filename
+
+
+# def move_log(res_path, dest_path):
+#     return ''
+
+
+def find_all_file(src_dir) -> list:
+    result = []
+    for i in Path(src_dir).iterdir():
+        if i.is_file():
+            result.append(i)
+        else:
+            for x in find_all_file(i):
+                result.append(x)
+    return result
+
+
+def check_csv(existing_file, csv_file):
+    exclude_list = []
+    for i in existing_file:
+        if i in csv_file:
+            exclude_list.append(i)
+    return exclude_list
+
+
+def get_theory_path(res_file, file_chapter, file_topic, file_name):
+    image_path = Path(res_file, file_chapter, file_topic, file_name)
+    # if not image_path.exists():
+    #    print(f"{image_path} not exists")
+    return image_path
+
+
+# def move_file(res_file, file_chapter, file_topic, file_name):
+#     return ''
+with open('res/picture.csv', newline='') as f:
+    reader = csv.reader(f)
+    theoty_path_list = []
+    for row in reader:
+        theoty_path_list.append(get_theory_path(
+            'res/pictures', row[0], row[1], row[3]))
+
+    for i in check_csv(find_all_file('res'), theoty_path_list):
+        print(f"{i.name} is not in place")
+
+
+# search_all_file(find_all_file("res/pictures"), "courthouse.jpg")
