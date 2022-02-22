60e94462c192b85377281cf692d2ba7f6eb2abdd
老房东 <huangdong@gmail.com>
2022-02-22 16:40:07 -0500

    更新词汇，为减少文件大小，去除json中的空格

--- utils/fileproc.py
+++ utils/fileproc.py
@@ -4,7 +4,8 @@ from json import dump
 
 def save_word_dict(filename, words_dict):
     with open(filename, 'w') as configfile:
-        dump(words_dict, configfile, indent=2, ensure_ascii=False)
+        # dump(words_dict, configfile, indent=2, ensure_ascii=False)
+        dump(words_dict, configfile, ensure_ascii=False)
 
 
 def gen_irregular_dict_from_csv(iverbs_csv_file, inous_csv_file, word_dict={}):
