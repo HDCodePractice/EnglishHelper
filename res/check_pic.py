28049b169897f93e6391ce7e59edf383d8c807a7
linbsd <linbsd@gmail.com>
2022-03-24 13:36:42 -0400

    修正了pixabay图片自动下载功能

--- res/check_pic.py
+++ res/check_pic.py
@@ -2,6 +2,7 @@ import csv
 from pathlib import Path
 import shutil
 import requests
+import json
 
 def unsplash_downloader(url, file_name):
 
@@ -17,25 +18,34 @@ def unsplash_downloader(url, file_name):
 
 def pixabay_downloader(url, file_name):
     
-    img_suffix = "jpg"
+    api_url = "https://pixabay.com/api/"
+    api_key = "26214455-eb3b048aff957b767ce9ed839"
     #url后面的‘/’不能少
     if not url.endswith("/"):
         url = url + "/"
     
     url_split = url.split('/')[-2]
     name_split = url_split.split('-')
+    pic_id = name_split[-1]
+        
+    #通过API获取下载链接
+    res = requests.get(api_url,params={
+                                   "key": api_key,
+                                   "id": pic_id
+                               })
 
-    #有一些是矢量图png
-    if url.find("vectors") == 0:
-        img_suffix = "png"
-    # 转换URL为下载链接
-    url_down = "https://pixabay.com/zh/images/download/" + name_split[1] + "-" + name_split[2] + "-" + name_split[-1] + "_640." + img_suffix + "?attachment"
-
-    res = requests.get(url_down, stream=True)
-    with open(file_name, 'wb') as f:
-        shutil.copyfileobj(res.raw, f)
-    print('Image sucessfully Downloaded: ', file_name)
-
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        #选取640尺寸的图片
+        real_pic_url = data["hits"][0]["webformatURL"]
+        pic_res = requests.get(real_pic_url)
+        with open(file_name, 'wb') as f:
+            f.write(pic_res.content)
+            f.close
+        print('Image sucessfully Downloaded: ', file_name)
+    else:
+        print("Cannot find this picture from pixabay")
+    
 
 def find_all_file(src_dir) -> list:
     ignores = [".DS_Store", "Thumbs.db"]
@@ -67,9 +77,9 @@ def get_theory_path(res_file, file_chapter, file_topic, file_name, down_link):
         if down_link.startswith("https://unsplash.com/photos/"):
             print(f"{image_path} not exists, downloading from unsplash...")
             unsplash_downloader(down_link, str(image_path))
-#        elif down_link.startswith("https://pixabay.com/"):
-#            print(f"{image_path} not exists, downloading from pixabay...")
-#            pixabay_downloader(down_link, str(image_path))
+        elif down_link.startswith("https://pixabay.com/"):
+            print(f"{image_path} not exists, downloading from pixabay...")
+            pixabay_downloader(down_link, str(image_path))
         else:
             print(f"{image_path} not exists, PLS download from {down_link}")
     return image_path
